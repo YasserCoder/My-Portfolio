3296044a5ffe140766a3e322eff678f09829a2ce
yasser <yasser.laiss01@gmail.com>
2024-10-22 21:58:47 +0100

    building education section

--- js/translations.js
+++ js/translations.js
@@ -20,7 +20,17 @@ const translations = {
         about_para1:
             "Hey! I am <strong class='font-bold'>Yasser Laissaoui</strong>, a 23-year-old front-end web developer from Algeria with a <strong class='font-bold' >License / Bachelor's Degree</strong> in Computer Science. I have a strong passion for creating dynamic and engaging user experiences on the web. Combining my technical skills with a creative mindset, I specialize in building responsive, visually appealing, and intuitive websites.",
         about_para2:
-            "With expertise in modern technologies like React, JavaScript, and Tailwind CSS, I’m dedicated to developing efficient, user-centered applications. I’m constantly learning and evolving in the fast-paced world of web development, and I’m excited to collaborate on projects that challenge my skills and bring innovativeideas to life.",
+            "With expertise in modern technologies like React, JavaScript, and Tailwind CSS, I’m dedicated to developing efficient, user-centered applications. I’m constantly learning and evolving in the fast-paced world of web development, and I’m excited to collaborate on projects that challenge my skills and bring innovative ideas to life.",
+        title_2020: "University",
+        para_2020:
+            "During my three years at university, I immersed myself in the world of Computer Science, gaining a strong foundation in programming and problem-solving. In 2023, I earned my license, equipped with the technical knowledge and curiosity needed to explore the ever-evolving field of web development.",
+        title_2023: "Front-end",
+        para_2023:
+            "Building on my university experience, I focused on front-end development by taking specialized courses and dedicating countless hours to self-learning. I sharpened my skills in HTML, CSS, JavaScript, and React, mastering the tools necessary to create dynamic, user-friendly websites.",
+        title_2024: "Real-World Projects",
+        para_2024:
+            "After solidifying my understanding of front-end concepts, I transitioned to real-world projects, bringing my ideas to life through practical applications. These hands-on experiences have allowed me to further hone my skills, delivering impactful and responsive web solutions that solve real user problems.",
+        date_2024: "2024-Now",
     },
     fr: {
         home: "Accueil",
@@ -44,6 +54,16 @@ const translations = {
             "Salut ! Je suis <strong class='font-bold'>Yasser Laissaoui</strong>, un développeur front-end de 23 ans originaire d'Algérie avec une <strong class='font-bold'>Licence en Informatique</strong>. J'ai une forte passion pour la création d'expériences utilisateurs dynamiques et engageantes sur le web. En combinant mes compétences techniques avec une mentalité créative, je me spécialise dans la création de sites web réactifs, visuellement attrayants et intuitifs.",
         about_para2:
             "Grâce à une expertise dans les technologies modernes comme React, JavaScript et Tailwind CSS, je suis dédié au développement d'applications efficaces et centrées sur l'utilisateur. Je suis constamment en train d'apprendre et d'évoluer dans le monde rapide du développement web, et je suis enthousiaste à l'idée de collaborer sur des projets qui défient mes compétences et apportent des idées innovantes à la vie.",
+        title_2020: "Université",
+        para_2020:
+            "Au cours de mes trois années universitaires, j'ai acquis une solide base en informatique, En 2023, j'ai obtenu ma licence, équipé des connaissances techniques et de la curiosité nécessaires pour explorer le domaine en constante évolution du développement web.",
+        title_2023: "Front-end",
+        para_2023:
+            "Fort de mon expérience universitaire, j'ai perfectionné mes compétences en développement front-end à travers des cours spécialisés et un auto-apprentissage approfondi, maîtrisant les outils essentiels pour créer des sites web dynamiques et conviviaux.",
+        title_2024: "Projets Réels",
+        para_2024:
+            "Après avoir solidifié mes connaissances en front-end, j'ai réalisé des projets concrets, transformant mes idées en applications pratiques et affinant mes compétences en développant des solutions web efficaces et réactives pour répondre aux besoins des utilisateurs.",
+        date_2024: "2024-Présent",
     },
     ar: {
         home: "الصفحة الرئيسية",
@@ -67,6 +87,16 @@ const translations = {
             "مرحباً! أنا <strong class='font-bold'>ياسر العيساوي</strong>، مطور واجهة أمامية عمره 23 عامًا من الجزائر، حاصل على <strong class='font-bold'>شهادة ليسانس في علوم الحاسوب</strong>. لدي شغف كبير بإنشاء تجارب مستخدم ديناميكية وجذابة على الويب. من خلال الجمع بين مهاراتي التقنية وعقليتي الإبداعية، أتخصص في بناء مواقع ويب متجاوبة، جذابة بصريًا، وبديهية.",
         about_para2:
             "بفضل خبرتي في التقنيات الحديثة مثل React و JavaScript و Tailwind CSS، أركز على تطوير تطبيقات فعالة تتمحور حول المستخدم. أنا دائمًا في حالة تعلم وتطور في العالم السريع لتطوير الويب، وأنا متحمس للتعاون في مشاريع تتحدى مهاراتي وتجلب أفكارًا مبتكرة إلى الحياة.",
+        title_2020: "الجامعة",
+        para_2020:
+            "خلال سنواتي الثلاث في الجامعة، انغمست في عالم علوم الحاسوب، مكتسبًا أساسًا قويًا في البرمجة وحل المشكلات. في عام 2023، حصلت على الشهادة، مجهزًا بالمعرفة التقنية والفضول اللازمين لاستكشاف المجال المتطور باستمرار لتطوير الويب.",
+        title_2023: "واجهة أمامية",
+        para_2023:
+            "بناءً على تجربتي الجامعية، ركزت على تطوير الواجهة الأمامية من خلال أخذ دورات متخصصة وتخصيص ساعات طويلة للتعلم الذاتي. لقد طورت مهاراتي في HTML و CSS و JavaScript و React، متقنًا الأدوات اللازمة لإنشاء مواقع ويب ديناميكية وسهلة الاستخدام.",
+        title_2024: "مشاريع واقعية",
+        para_2024:
+            "بعد تعزيز فهمي لمفاهيم الواجهة الأمامية، انتقلت إلى المشاريع الواقعية، محولاً أفكاري إلى تطبيقات عملية. هذه التجارب العملية سمحت لي بصقل مهاراتي بشكل أكبر وتقديم حلول ويب فعالة ومتجاوبة تحل مشاكل المستخدمين الحقيقية.",
+        date_2024: "إلى لآن-2024",
     },
 };
 
